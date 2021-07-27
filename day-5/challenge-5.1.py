b0b5d230d994f4e21d23015136ce17d085ead9d1
swetank01 <iamswetanksoni@gmail.com>
2021-07-27 12:43:24 +0530

    re-build and commit

--- day-5/challenge-5.1.py
+++ day-5/challenge-5.1.py
@@ -1,16 +1,13 @@
 # Average Height
 # Don't change the code below 
-
 student_heights = input("Input a list  of student Heights: ").split(" ")
-# Don't change the code above  
-total_ht = 0
 for n in range(0, (len(student_heights))):
     student_heights[n] = int(student_heights[n])
-    total_ht += student_heights[n]
-print(student_heights)
+# Don't change the code above  
 
-student_heights_avg = total_ht / int(len(student_heights) - 1)
+total_height = 0
+for students in student_heights:
+    total_height += students
 
-print(total_ht,len(student_heights))
-
-print(student_heights_avg)
+avg_height = total_height / len(student_heights)
+print(round(avg_height))
---
+++ day-5/challenge-5.2.py
@@ -0,0 +1,10 @@
+# Height Score
+# Don't change the code below 
+student_heights = input("Input a list  of student Score in Maths ").split(" ")
+for n in range(0, (len(student_heights))):
+    student_heights[n] = int(student_heights[n])
+# Don't change the code above  
+
+
+
+     
